348d5802a7b2cbec89612473d562b61022bf252a
zwwgit <1565280103@qq.com>
2020-04-30 15:24:45 +0800

    1

--- build/utils.js
+++ build/utils.js
@@ -116,7 +116,7 @@ var merge = require('webpack-merge')
 // 通过glob模块读取pages文件夹下的所有对应文件夹下的js后缀文件，如果该文件存在
 // 那么就作为入口处理
 exports.entries = function () {
-    var entryFiles = glob.sync(PAGE_PATH + '/*/*.js')
+    var entryFiles = glob.sync(PAGE_PATH + '/**/*.js')
     var map = {}
     entryFiles.forEach((filePath) => {
         var filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
@@ -128,13 +128,13 @@ exports.entries = function () {
 //多页面输出配置
 // 与上面的多页面入口配置相同，读取pages文件夹下的对应的html后缀文件，然后放入数组中
 exports.htmlPlugin = function () {
-    let entryHtml = glob.sync(PAGE_PATH + '/*/*.html')
+    let entryHtml = glob.sync(PAGE_PATH + '/**/*.vue')
     let arr = []
     entryHtml.forEach((filePath) => {
         let filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
         let conf = {
             // 模板来源
-            template: filePath,
+            template: 'index.html',
             // 文件名称
             filename: filename + '.html',
             // 页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有的js脚本
